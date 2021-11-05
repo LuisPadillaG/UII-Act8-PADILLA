3d7fa8f7b0a8ad1eb6eaa81619f2a0e39a6dac25
Luis Alberto Padilla González <91347620+LuisPadillaG@users.noreply.github.com>
2021-11-05 11:32:47 -0600

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
 					<ul>
 						<li><a href="empleado.php">Formulario Empleado</a></li>
 						<li><a href="cliente.php">Formulario Cliente</a></li>
-					    <img src="img/infohotel.png" alt="FalconMasters" width="230"></a>
+					    <img src="infohotel.png" alt="FalconMasters" width="230"></a>
 					</ul>
 				</div>
 			</aside>
